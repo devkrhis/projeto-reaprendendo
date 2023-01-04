86d5c89e61ff9760e69b1ce675db8542a64a1d05
krhistopher <krhistopher@weon.com.br>
2023-01-04 17:54:08 -0300

    aula 122 finalizada novamente

--- src/components/exportandoComponente.vue
+++ src/components/exportandoComponente.vue
@@ -2,21 +2,43 @@
     <div id="usuario">
         <h1> Controle de Usuário: {{ name }}</h1>
         <button type="button" @click="changeName"> Change Name </button>
+        <hr>
+        <segundoComponenteExportado :valueMagic="valueMagic" :oldValue="oldValue"/>
+        <button type="button" @click="changeValue"> Change Value </button>
+        <button type="button" @click="changeMenorValue"> Diminuindo Valor</button>
+        <segundoComponenteExportado :valueMagic="valueMagic" :oldValue="oldValue"/>
+        <segundoComponenteExportado :valueMagic="valueMagic" :oldValue="oldValue"/>
+        <segundoComponenteExportado :valueMagic="valueMagic" :oldValue="oldValue"/>
     </div>
     
 </template>
 
 <script>
+
+import segundoComponenteExportado from './segundoComponenteExportado.vue';
+
 export default {
+    components: {
+        segundoComponenteExportado
+    },
     data(){
         return{
             name: 'Krhistopher',
+            valueMagic: 0,
+            oldValue: 100
         }
     },
     methods: {
         changeName(){
                 this.name = 'Lucas'
-            }
+            },
+        changeValue(){
+            this.valueMagic++
+
+        },
+        changeMenorValue(){
+            this.oldValue--
+        }
     }
     
 }
--- src/components/segundoComponenteExportado.vue
+++ src/components/segundoComponenteExportado.vue
@@ -1,9 +1,15 @@
 <template>
+    <div id="contadorMagico">
+        <h2> O valor é: {{ valueMagic }}</h2>
+        <h2> O valor Negativo é: {{ oldValue }}</h2>
+        
+    </div>
     
 </template>
 
 <script>
 export default {
+    props: ['valueMagic','oldValue']
     
 }
 </script>
